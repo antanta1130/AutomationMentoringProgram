412fa1161c2f8b3b01ad3d9d22a41d3d9f3637eb
tetiana kotova <tetiana_kotova@epam.com>
2017-02-15 10:06:59 +0200

    fix

--- src/com/tasks/Task1.java
+++ src/com/tasks/Task1.java
@@ -5,7 +5,9 @@ package com.tasks;
  */
 public class Task1 {
     public static void main(String[] args) {
-        double a = 1, b = - 2, c = 1;
+        double a = 1;
+        double b = - 2;
+        double c = 1;
         double x1, x2;
         double D = b * b - 4 * a * c;
 
