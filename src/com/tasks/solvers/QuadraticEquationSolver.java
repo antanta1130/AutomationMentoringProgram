4a2052b231424b6c68e4ec92f08d103dd5c03de2
tetiana kotova <tetiana_kotova@epam.com>
2017-02-22 11:12:07 +0200

    getters and setters added

--- src/com/tasks/solvers/QuadraticEquationSolver.java
+++ src/com/tasks/solvers/QuadraticEquationSolver.java
@@ -38,4 +38,27 @@ public class QuadraticEquationSolver implements EquationSolver {
         }
     }
 
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
 }
