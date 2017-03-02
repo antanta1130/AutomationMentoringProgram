99e2c86376e2bc92e35e64741b7405e0503b63f5
tetiana kotova <tetiana_kotova@epam.com>
2017-03-02 15:08:23 +0200

    start working with testNG

--- src/test/com/tasks/solvers/LinearEquationSolverTest2.java
+++ src/test/com/tasks/solvers/LinearEquationSolverTest2.java
@@ -4,11 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LinearEquationSolverTest2 {
-    private LinearEquationSolver equation = new LinearEquationSolver(2, -4);
+    private EquationSolver linearEquationSolver1 = new LinearEquationSolver(3, 8);
+    private EquationSolver linearEquationSolver2 = new LinearEquationSolver(0, 8);
+    private EquationSolver linearEquationSolver3 = new LinearEquationSolver();
 
     @Test
     public void testSolve() {
-        Assert.assertEquals("Root of a linear equation is: x = 2.000", equation.solve());
+        Assert.assertEquals("Root of a linear equation is: x = 2.000", linearEquationSolver1.solve());
+    }
+
+    @Test
+    public void testSolveWithIncorrectParameters() {
+        Assert.assertEquals("It is not a linear equation", linearEquationSolver2.solve());
+    }
+
+    @Test
+    public void testSolveWithConstructingObjectUsingDefaultConstructor() {
+        Assert.assertEquals("It is not a linear equation", linearEquationSolver3.solve());
     }
 
 }
---
+++ src/test/com/tasks/solvers/QuadraticEquationSolverTest.java
@@ -0,0 +1,32 @@
+package com.tasks.solvers;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class QuadraticEquationSolverTest {
+    private EquationSolver quadraticEquationSolver1 = new QuadraticEquationSolver(8, -77, 4);
+    private EquationSolver quadraticEquationSolver2 = new QuadraticEquationSolver(0, 77, 4);
+    private EquationSolver quadraticEquationSolver3 = new QuadraticEquationSolver(8, 1, 4);
+    private EquationSolver quadraticEquationSolver4 = new QuadraticEquationSolver();
+
+    @Test
+    public void testSolve() {
+        Assert.assertEquals("Roots of a quadratic equation are: x1 = 9.573; x2 = 0.052", quadraticEquationSolver1.solve());
+    }
+
+    @Test
+    public void testSolveWithDivisionByZeroCase() {
+        Assert.assertEquals("It is not a quadratic equation", quadraticEquationSolver2.solve());
+    }
+
+    @Test
+    public void testSolveWithDiscriminantLessThanZero() {
+        Assert.assertEquals("There are no real roots, discriminant = -127.0", quadraticEquationSolver3.solve());
+    }
+
+    @Test
+    public void testSolveWithConstructingObjectUsingDefaultConstructor() {
+        Assert.assertEquals("It is not a quadratic equation", quadraticEquationSolver4.solve());
+    }
+
+}
