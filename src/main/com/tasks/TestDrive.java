8c7e7d7846defa6b0c41546e08904aa54ccca811
tetiana kotova <tetiana_kotova@epam.com>
2017-03-02 17:17:21 +0200

    refactoring

--- src/main/com/tasks/TestDrive.java
+++ src/main/com/tasks/TestDrive.java
@@ -1,32 +1,10 @@
 package com.tasks;
 
-import com.tasks.task2.solvers.EquationSolver;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-import static com.tasks.task2.decisionmaker.DecisionMaker.chooseTypeOfEquation;
+import com.tasks.task2.decisionmaker.DecisionMaker;
 
 public class TestDrive {
     public static void main(String[] args) {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        EquationSolver equationSolver = null;
-        String type;
-
-        try {
-            while (equationSolver == null) {
-                System.out.println("Enter type of an equation which you would like to solve: linear/quadratic");
-                type = br.readLine();
-                equationSolver = chooseTypeOfEquation(type);
-            }
-
-            equationSolver.readParametersfromConsole();
-            System.out.println(equationSolver.toString());
-            System.out.println(equationSolver.solve() + "\n");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        DecisionMaker decisionMaker = new DecisionMaker();
+        decisionMaker.readTypeOfEquationFromConsole();
     }
 }
