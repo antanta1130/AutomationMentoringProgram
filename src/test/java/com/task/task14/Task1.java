01e311e6e4d5ad7bab1885a108de10e188c4e402
tetiana kotova <tetiana_kotova@epam.com>
2017-08-02 18:43:03 +0300

    task 14 - 2 wip

--- src/test/java/com/task/task14/Task1.java
+++ src/test/java/com/task/task14/Task1.java
@@ -1,13 +1,26 @@
 package com.task.task14;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
@@ -18,15 +31,16 @@ public class Task1 {
 
     @Before
     public void createDriver() {
-        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Tetiana\\Documents\\geckodriver.exe");
-        driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver", "D:\\AutomationMentoringProgram\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
         wait = new FluentWait<WebDriver>(driver);
         wait.withTimeout(5, TimeUnit.SECONDS).pollingEvery(500, TimeUnit.MILLISECONDS);
     }
 
     @After
     public void quitDriver() {
-        driver.quit();
+        //driver.quit();
     }
 
     @Test
@@ -40,7 +54,46 @@ public class Task1 {
         driver.findElement(By.id("pass")).sendKeys("1q2w3e4r5t");
 
         driver.findElement(By.xpath("//input[@type='submit']")).click();
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("userNav")));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='blueBarDOMInspector']//a[@data-testid='blue_bar_profile_link']/span")));
+
+    }
+    
+    @Test
+    public void takeScreenShots() throws Throwable {
+        driver.get("https://en.wikipedia.org/wiki/Main_Page");
+        
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@id='mp-left']/tbody/tr[4]//img")));
+        WebElement image = driver.findElement(By.xpath("//table[@id='mp-left']/tbody/tr[4]//img"));
+        WebElement helper = driver.findElement(By.xpath("//*[@id='mp-tfa-img']/div/a/img"));
+        
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", helper);
+        Thread.sleep(800); 
+
+        
+        File scr1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        
+        try {
+			FileUtils.copyFile(scr1, new File("D:\\AutomationMentoringProgram\\screenShots\\" + System.currentTimeMillis() + ".png" ));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        BufferedImage fullImg = ImageIO.read(scr1);
+
+        Point point = image.getLocation();
+        
+        System.out.println(point.getX() + " " + point.getY());
+
+        int elementWidth = image.getSize().getWidth();
+        int elementHeight = image.getSize().getHeight();
+
+        BufferedImage elementScreenshot = fullImg.getSubimage(point.getX(), point.getY(), elementWidth, elementHeight);
+        ImageIO.write(elementScreenshot, "png", scr1);
+        
+        try {
+			FileUtils.copyFile(scr1, new File("D:\\AutomationMentoringProgram\\screenShots\\" + System.currentTimeMillis() + ".png" ));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
     }
 
