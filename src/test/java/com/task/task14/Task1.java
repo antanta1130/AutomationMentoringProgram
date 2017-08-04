4a30544bb90d9e56bfe8011030d92af8de1c4bb7
tetiana kotova <tetiana_kotova@epam.com>
2017-08-04 18:05:38 +0300

    Task1, Task2

--- src/test/java/com/task/task14/Task1.java
+++ src/test/java/com/task/task14/Task1.java
@@ -3,6 +3,7 @@ package com.task.task14;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import javax.imageio.ImageIO;
@@ -12,6 +13,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Point;
@@ -19,8 +21,6 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 
@@ -28,6 +28,7 @@ public class Task1 {
 
     private WebDriver driver;
     private FluentWait<WebDriver> wait;
+    private FluentWait<WebDriver> waitAd;
 
     @Before
     public void createDriver() {
@@ -36,11 +37,14 @@ public class Task1 {
         driver.manage().window().maximize();
         wait = new FluentWait<WebDriver>(driver);
         wait.withTimeout(5, TimeUnit.SECONDS).pollingEvery(500, TimeUnit.MILLISECONDS);
+        
+        waitAd = new FluentWait<WebDriver>(driver);
+        waitAd.withTimeout(5, TimeUnit.SECONDS).pollingEvery(500, TimeUnit.MILLISECONDS);
     }
 
     @After
     public void quitDriver() {
-        //driver.quit();
+        driver.quit();
     }
 
     @Test
@@ -62,38 +66,39 @@ public class Task1 {
     public void takeScreenShots() throws Throwable {
         driver.get("https://en.wikipedia.org/wiki/Main_Page");
         
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//table[@id='mp-left']/tbody/tr[4]//img")));
-        WebElement image = driver.findElement(By.xpath("//table[@id='mp-left']/tbody/tr[4]//img"));
-        WebElement helper = driver.findElement(By.xpath("//*[@id='mp-tfa-img']/div/a/img"));
-        
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", helper);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='mp-left']//img")));
+        WebElement image = driver.findElement(By.xpath("//*[@id='mp-left']//img"));
+ 
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", image);
         Thread.sleep(800); 
-
         
-        File scr1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
-        
-        try {
-			FileUtils.copyFile(scr1, new File("D:\\AutomationMentoringProgram\\screenShots\\" + System.currentTimeMillis() + ".png" ));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        BufferedImage fullImg = ImageIO.read(scr1);
+        try{
+        	waitAd.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='B1718_0721_enUA_dsk_p1_lg_seq_A']//span[contains(@class, 'frb-close')]")));
+        	driver.findElement(By.xpath("//*[@id='B1718_0721_enUA_dsk_p1_lg_seq_A']//span[contains(@class, 'frb-close')]")).click();
+        }
+        finally{
+            File scr1 = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            
+            try {
+    			FileUtils.copyFile(scr1, new File("D:\\AutomationMentoringProgram\\screenShots\\" + System.currentTimeMillis() + ".png" ));
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}
+            
+            BufferedImage fullImg = ImageIO.read(scr1);
+            Point point = image.getLocation();
+            Dimension size = image.getSize();
 
-        Point point = image.getLocation();
-        
-        System.out.println(point.getX() + " " + point.getY());
+            BufferedImage elementScreenshot = fullImg.getSubimage(point.getX(), 0, size.getWidth(), size.getHeight());
+            ImageIO.write(elementScreenshot, "png", scr1);
+            
+            try {
+    			FileUtils.copyFile(scr1, new File("D:\\AutomationMentoringProgram\\screenShots\\" + System.currentTimeMillis() + ".png" ));
+    		} catch (IOException e) {
+    			e.printStackTrace();
+    		}	
+        }
 
-        int elementWidth = image.getSize().getWidth();
-        int elementHeight = image.getSize().getHeight();
-
-        BufferedImage elementScreenshot = fullImg.getSubimage(point.getX(), point.getY(), elementWidth, elementHeight);
-        ImageIO.write(elementScreenshot, "png", scr1);
-        
-        try {
-			FileUtils.copyFile(scr1, new File("D:\\AutomationMentoringProgram\\screenShots\\" + System.currentTimeMillis() + ".png" ));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 
     }
 
